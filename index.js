b2d3ebb1b092d9ec37c35272477b6a19e2c17746
Abdullateef Abdullateef <abdullateefabdull2364@gmail.com>
2022-06-25 19:56:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let num1 = parseFloat("10");
+let num2 = parseFloat("15");
+
+
+
+ function sum(num1, num2) {
+    return num1 + num2;
+
+}
+console.log(num1 + num2);
+
+ function subtract(num1, num2) {
+    return num1 - num2;
+}
+console.log(num1 - num2);
+
+function multiply(num1, num2) {
+    return num1 * num2;
+
+}
+console.log(num1 * num2);
+
+function divide(num1, num2) {
+    return num1 / num2;
+
+}
+console.log(num1 / num2);
